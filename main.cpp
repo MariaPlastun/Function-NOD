31b4ff032573fa65e2307cce35b6e38b74a6ff65
MariaPlastun <63410203+MariaPlastun@users.noreply.github.com>
2020-04-13 12:45:46 +0500

    Создать main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "app.h"
+
+using namespace std;
+
+int main()
+{
+	int k = 15, s = 25;
+	cout << nod(k, s) << endl;
+	long k = 15, s = 25;
+	cout << nod(k, s) << endl;
+	float k = 15, s = 25;
+	cout << nod(k, s) << endl;
+	double k = 15.5, s = 25.5;
+	cout << nod(k, s) << endl;
+	return 0;
+}
+
